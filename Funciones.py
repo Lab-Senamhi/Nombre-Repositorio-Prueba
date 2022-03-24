42e0dc9d5eeb6223c362e670d2f79de0ed3e296c
Laboratorio Termohigrometría <jenrique@senamhi.gob.pe>
2022-03-24 12:36:01 -0500

    Agrege la funcion mul

--- Funciones.py
+++ Funciones.py
@@ -4,3 +4,7 @@ x = int(input('Ingrese el primer numero: '))
 y = int(input('Ingrese el segundo numero: '))
 z = suma(x,y)
 print('la suma es: ',z)
+def mul(a,b):
+    return a*b
+w = mul(x,y)
+print('El producto es: ',w)
