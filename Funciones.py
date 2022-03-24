a424cae918270eb70a5c36929f67534d103dcc10
Laboratorio Termohigrometría <jenrique@senamhi.gob.pe>
2022-03-24 12:33:53 -0500

    Se hizo agregos al py

--- Funciones.py
+++ Funciones.py
@@ -1,4 +1,6 @@
 def suma(a,b):
     return a+b
-x = suma(6,8)
-print(x)
+x = int(input('Ingrese el primer numero: '))
+y = int(input('Ingrese el segundo numero: '))
+z = suma(x,y)
+print('la suma es: ',z)
